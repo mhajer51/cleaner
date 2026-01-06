78aeeec903dde807e2af4401a53583336d79b8f7
mhajer51 <mhajer@netronsolutions.com>
2026-01-06 17:02:04 +0400

    Restrict authentication to admin login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Admin\AuthenticatedSessionController as AdminAuthenticatedSessionController;
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SliderController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -10,22 +9,7 @@ Route::get('/', function () {
     return Inertia::render('App');
 });
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboards/default', function () {
-        return Inertia::render('App');
-    })->name('dashboard');
-
-    Route::get('/admin/slider', [SliderController::class, 'index'])
-        ->name('admin.slider.index');
-    Route::post('/admin/slider', [SliderController::class, 'store'])
-        ->name('admin.slider.store');
-});
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::redirect('/login', '/admin/login');
 
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::middleware('guest:admin')->group(function () {
@@ -40,12 +24,15 @@ Route::prefix('admin')->name('admin.')->group(function () {
             return Inertia::render('App');
         })->name('dashboard');
 
+        Route::get('/slider', [SliderController::class, 'index'])
+            ->name('slider.index');
+        Route::post('/slider', [SliderController::class, 'store'])
+            ->name('slider.store');
+
         Route::post('/logout', [AdminAuthenticatedSessionController::class, 'destroy'])
             ->name('logout');
     });
 });
-
-require __DIR__.'/auth.php';
 
 Route::get('/{any}', function () {
     return Inertia::render('App');
